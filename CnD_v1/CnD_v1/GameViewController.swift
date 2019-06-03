5b385abbb6d30a61c8af69ce83b08c0604b40308
Sweeny <m.sweeny.gamer@live.ca>
2019-06-03 13:51:08 -0400

    Initial push

---
+++ CnD_v1/CnD_v1/GameViewController.swift
@@ -0,0 +1,29 @@
+//
+//  GameViewController.swift
+//  CnD_v1
+//
+//  Created by Michael Sweeny on 2019-06-02.
+//  Copyright © 2019 Michael Sweeny. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        let scene = GameScene(size:CGSize(width: 2048, height: 1536))
+        let skView = self.view as! SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        skView.ignoresSiblingOrder = true
+        scene.scaleMode = .aspectFill
+        skView.presentScene(scene)
+    }
+    override var prefersStatusBarHidden: Bool
+    {
+            return true
+    }
+}
---
+++ CnD_v1/CnD_v1Tests/CnD_v1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  CnD_v1Tests.swift
+//  CnD_v1Tests
+//
+//  Created by Michael Sweeny on 2019-06-02.
+//  Copyright © 2019 Michael Sweeny. All rights reserved.
+//
+
+import XCTest
+@testable import CnD_v1
+
+class CnD_v1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
