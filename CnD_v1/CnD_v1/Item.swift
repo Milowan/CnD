4e3a0c7d841b15652e7b0109229f370595e8a36d
Gavin McGuire <milo@alienware-strimen.trios.com>
2019-06-18 13:30:45 -0400

    added doors and made items

---
+++ CnD_v1/CnD_v1/Item.swift
@@ -0,0 +1,19 @@
+//
+//  Item.swift
+//  CnD_v1
+//
+//  Created by Gavin McGuire on 2019-06-18.
+//  Copyright © 2019 Michael Sweeny. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class Item
+{
+    var sprite : SKSpriteNode
+    init (s : SKSpriteNode)
+    {
+        sprite = s
+    }
+}
