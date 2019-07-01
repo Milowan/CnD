a391f6fbe71699dab595225eee620189566a714b
Sweeny <m.sweeny.gamer@live.ca>
2019-07-01 13:34:12 -0400

    ALL COMBAT ANIMATIONS!!!!
    -for basic skeletons at least........

--- CnD_v1/CnD_v1/Entity/Enemy/LesserSkeleton.swift
+++ CnD_v1/CnD_v1/Entity/Enemy/LesserSkeleton.swift
@@ -16,6 +16,7 @@ class LesserSkeleton : Enemy
     var eiLTextures : [SKTexture] = []
     var enemyIdleR : SKAction?
     var eiRTextures : [SKTexture] = []
+    var tempTimer = 0
     
     var enemyAtkR : SKAction?
     var eatkRTextures : [SKTexture] = []
@@ -27,7 +28,8 @@ class LesserSkeleton : Enemy
     var enemyDeathL : SKAction?
     var edLTextures : [SKTexture] = []
     
-    let animSpeed = 0.1
+    let animSpeed = 0.3
+    let atkAnimSp = 0.1
     var newCombat = true
     
     init (x : Int, y : Int, z : Int, s : SKSpriteNode)
@@ -55,75 +57,69 @@ class LesserSkeleton : Enemy
     override func update()
     {
         super.update()
-        if !isAlive && player!.cDir == 0
+        if isAlive
         {
-            runAnimation(animAction: enemyDeathL!, animKey: "death")
-            if !sprite!.actionForKeyIsRunning(key: "death")
-            {
-                isDespawned = true
-            }
-        }
-        else
-        {
-            if inCombat && newCombat
-            {
-                startAnimation(animAction: enemyIdleL!, animKey: "idleL")
-                newCombat = false
-            }
             if inCombat
             {
-                let atkCD = player!.combatTimer % ats
-                if atkCD == 0
+                if player!.cDir == 0 // player is on your left
                 {
-                    if !sprite!.actionForKeyIsRunning(key: "atkLeft")
+                    if !sprite!.actionForKeyIsRunning(key: "idleL") || !sprite!.actionForKeyIsRunning(key: "atkLeft")
+                    {
+                        startAnimation(animAction: enemyIdleL!, animKey: "idleL")
+                    }
+                    let atkCD = player!.combatTimer % ats
+                    if atkCD == 0
                     {
                         self.runAnimation(animAction: enemyAtkL!, animKey: "atkLeft")
                         if arc4random_uniform( (UInt32(50))) > player!.calcTotalEVS() / prs
                         {
                             player!.dmgTaken += dmg / player!.calcTotalDEF()
-                            newCombat = true
                         }
                     }
                 }
-            }
-        }
-        if !isAlive && player!.cDir == 1
-        {
-            runAnimation(animAction: enemyDeathL!, animKey: "death")
-            if !sprite!.actionForKeyIsRunning(key: "death")
-            {
-                isDespawned = true
-            }
-        }
-        else
-        {
-            if inCombat && newCombat
-            {
-                    startAnimation(animAction: enemyIdleL!, animKey: "idleR")
-                    newCombat = false
-                }
-                if inCombat
+                if player!.cDir == 1 // player is on your right
                 {
+                    if !sprite!.actionForKeyIsRunning(key: "idleR") || !sprite!.actionForKeyIsRunning(key: "atkRight")
+                    {
+                        startAnimation(animAction: enemyIdleR!, animKey: "idleR")
+                    }
                     let atkCD = player!.combatTimer % ats
                     if atkCD == 0
                     {
-                        if !sprite!.actionForKeyIsRunning(key: "atkRight")
+                        self.runAnimation(animAction: enemyAtkR!, animKey: "atkRight")
+                        if arc4random_uniform( (UInt32(50))) > player!.calcTotalEVS() / prs
                         {
-                            self.runAnimation(animAction: enemyAtkR!, animKey: "atkRight")
-                            if arc4random_uniform( (UInt32(50))) > player!.calcTotalEVS() / prs
-                            {
-                                player!.dmgTaken += dmg / player!.calcTotalDEF()
-                                newCombat = true
-                            }
+                            player!.dmgTaken += dmg / player!.calcTotalDEF()
                         }
                     }
                 }
             }
         }
-        
-    
-    
-    
+        if !isAlive && inCombat
+        {
+            tempTimer += 1
+            if tempTimer == 8
+            {
+                if player!.cDir == 0
+                {
+                    runAnimation(animAction: enemyDeathL!, animKey: "death")
+                    stopAnimation(animKey: "idleL")
+                    stopAnimation(animKey: "idleR")
+                }
+                if player!.cDir == 1
+                {
+                    runAnimation(animAction: enemyDeathR!, animKey: "death")
+                    stopAnimation(animKey: "idleR")
+                    stopAnimation(animKey: "idleL")
+                }
+            }
+            if tempTimer == 30
+            {
+                isDespawned = true
+                tempTimer = 0
+            }
+        }
+    }   
     
     func setAnimations()
     {
@@ -155,12 +151,12 @@ class LesserSkeleton : Enemy
         enemyIdleR = SKAction.animate(with: eiRTextures, timePerFrame: animSpeed)
         enemyIdleL = SKAction.animate(with: eiLTextures, timePerFrame: animSpeed)
         
-        enemyAtkR = SKAction.animate(with: eatkRTextures, timePerFrame: animSpeed)
-        var tempAnim = SKAction.animate(with: eatkLTextures, timePerFrame: animSpeed)
+        enemyAtkR = SKAction.animate(with: eatkRTextures, timePerFrame: atkAnimSp)
+        var tempAnim = SKAction.animate(with: eatkLTextures, timePerFrame: atkAnimSp)
         enemyAtkL = tempAnim.reversed()
         
-        enemyDeathR = SKAction.animate(with: edRTextures, timePerFrame: animSpeed)
-        tempAnim = SKAction.animate(with: edLTextures, timePerFrame: animSpeed)
+        enemyDeathR = SKAction.animate(with: edRTextures, timePerFrame: atkAnimSp)
+        tempAnim = SKAction.animate(with: edLTextures, timePerFrame: atkAnimSp)
         enemyDeathL = tempAnim.reversed()
         
     }
